92d1128180c774230e89690fdb6aa762390f0264
dawidCze <dawidcze8@gmail.com>
2021-05-10 22:46:44 +0200

    chat history added

--- zad9_chat_socket_io/serve.js
+++ zad9_chat_socket_io/serve.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-let user;
+let history = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,23 +13,22 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   socket.on('add user', (val) => {
+    socket.userName = val;
     console.log(val);
-    user=val;
-    io.emit('user joined', {msguser: user});
+    io.emit('user joined', {msguser: socket.userName});
+    io.emit('send history', {data: history, userh: socket.userName});
   });
   
   socket.on('disconnect', () => {
-    console.log('user disconnected');
-    //io.emit('chat message', 'user disconnected');
+    io.emit('user disconnect', {msguser: socket.userName});
   });
 
-  
   socket.on('chat message', (data) => {
     io.emit('chat message', {msg: data.msg,msguser: data.msguser});
+    history.push({msg: data.msg,msguser: data.msguser});
+    //console.log(history);
   });
 });
-
-
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
